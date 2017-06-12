f38aa4614f59a7c18fb333c0c0cb90a3d8983a0b
moshe malka <moshemalka2014@gmail.com>
2017-06-13 01:07:52 +0300

    Add files via upload
    work in progress...

---
+++ shortz.py
@@ -0,0 +1,25 @@
+
+def Shortz(num):
+    if(num>0):print num
+    if(num==1 or num==0):
+        print "END"
+    elif(num%2==0):
+        n1=(num//4)
+        Shortz(n1)
+    else:
+        n2=((3*num)+1)
+        Shortz(n2) 
+
+def ShortzSum(num):
+    m_sum=num
+    if(num%2==0):
+        n1=(num//4)
+        m_sum+=n1
+        ShortzSum(n1)
+    elif(num==1):
+        m_sum+=1
+        print m_sum
+    else:
+        n2=((3*num)+1)
+        m_sum+=n2
+        ShortzSum(n2)
