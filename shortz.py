b79349ffbe23e5a5db25ebb4d06f099cd936b7bd
moshe malka <moshemalka2014@gmail.com>
2017-06-14 23:01:34 +0300

    Create shortz.py
    Finished.
    INFO
    CONTACT US
    RULES
    
    LOGOUT
    SOLVE CHALLENGES AND GET AS MANY POINTS AS YOU CAN!
    YOUR TOTAL SCORE IS: 20
    Reversing
    Simple Machine
    40 points
    Hide and Seek
    30 points
    Web
    All Your Base
    10 points
    Fortune Checker
    10 points
    Networking
    Sniffing Cap
    80 points
    Try This One
    20 points
    Logic
    Bovinescatology Cipher
    30 points
    Animal Cruelty
    20 points
    Programming
    Arnie's Secret
    30 points
    Shortz
    10 points
    Mixed
    The Black Box
    50 points
    Lost Decompressor
    100 points
    SOLVE CHALLENGES, AND WIN AN OPPORTUNITY TO BECOME A CYBER EXPERT IN CHECK POINT
    IN ADDITION, 3 GOPROS WILL BE GIVEN AWAY TO TOP SOLVERS
    
    Shortz
    One slightly famous conjecture in math which hasn't been proved yet is called Collatz: 
    https://en.wikipedia.org/wiki/Collatz_conjecture
    We found a locked device for which the one-time password is generated using a variation of this algorithm. 
    To break it, we want to ask for your help in generating one of the one-time password.
    The device uses a variation of the Collatz conjecture on INTEGER numbers. 
    We'll call it Shortz.
    What we know: 
    1. We know the device ALWAYS divides by 4 on even numbers, even ones not divisible by 4, so the next step after 6 would be 6/4 == 1. 
    2. We know the device always reached a stop condition even with the alternative behaviour, not sure if there was another change in the code...
    The device uses two functions when generating passwords: 
    1. Shortz(n) -> the number of steps from n until we reach a stop condition 
    2. ShortzSum(n) -> the sum of all the steps in Shortz(n) including n itself
    Here is an example: 
    Calculating Shortz(5) would have the following values: 
    5 
    16 
    4 
    1 
    Thus ShortzSum(5) is 26, while Shortz(5) is 3 (5->16->4->1)
    Help us break the device's password!
    Calculate this number: 
    ShortzSum(517612164044564) * Shortz(98325112)
    Send us the resulting one-time token.
    
    
    
    
    already solved
    10
    PTS
    category
    Programming

--- shortz.py
+++ shortz.py
@@ -1,25 +1,32 @@
 
-def Shortz(num):
-    if(num>0):print num
+def _shortz(num):
     if(num==1 or num==0):
-        print "END"
-    elif(num%2==0):
-        n1=(num//4)
-        Shortz(n1)
-    else:
-        n2=((3*num)+1)
-        Shortz(n2) 
-# <Not Finished>
-def ShortzSum(num):
-    m_sum=num
+        return num
     if(num%2==0):
-        n1=(num//4)
-        m_sum+=n1
-        ShortzSum(n1)
-    elif(num==1):
-        m_sum+=1
-        print m_sum
-    else:
-        n2=((3*num)+1)
-        m_sum+=n2
-        ShortzSum(n2)
+        return (num//4)
+    if(num%2==1):
+        return ((3*num)+1)
+
+
+def Shortz(n):
+    counter=0
+    while True:
+        counter+=1
+        n=_shortz(n)
+        if(n==0 or n==1):
+            break
+    return int(counter)
+
+def ShortzSum(num):
+    sum1=0
+    out=[]
+    out.append(num)
+    while True:
+        num=_shortz(num)
+        out.append(num)
+        if(num==1 or num==0):
+            break
+    #print out
+    for o in out:
+        sum1+=o
+    return int(sum1)
